bcb765d78e11d73792128755d56af4e22c661f7a
patrickmao93 <patrickmao93@gmail.com>
2019-07-22 15:07:56 +0800

    init

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -12,33 +12,30 @@ const options = {
 const web3 = new Web3(ganache.provider(), null, options);
 
 let accounts;
-let inbox;
+let lottery;
 
 beforeEach(async () => {
   // get a list of all accounts
   accounts = await web3.eth.getAccounts();
   // use one of those accounts to deploy the contract
-  inbox = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({
-      data: bytecode,
-      arguments: ["Hi there!"]
-    })
+  lottery = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode })
     .send({ from: accounts[0], gas: "1000000" });
 });
 
-describe("Inbox", () => {
+describe("Lottery", () => {
   it("deploys a contract", () => {
-    assert.ok(inbox.options.address);
+    assert.ok(lottery.options.address);
   });
 
-  it("has a default message", async () => {
-    const message = await inbox.methods.message().call();
-    assert.equal(message, "Hi there!");
-  });
+  // it("has a default message", async () => {
+  //   const message = await lottery.methods.message().call();
+  //   assert.equal(message, "Hi there!");
+  // });
 
-  it("can change the message", async () => {
-    await inbox.methods.setMessage("Bye there!").send({ from: accounts[0] });
-    const message = await inbox.methods.message().call();
-    assert.equal(message, "Bye there!");
-  });
+  // it("can change the message", async () => {
+  //   await lottery.methods.setMessage("Bye there!").send({ from: accounts[0] });
+  //   const message = await lottery.methods.message().call();
+  //   assert.equal(message, "Bye there!");
+  // });
 });
